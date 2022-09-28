159fa51bfad2d1f4736567913cacae8b19a8a966
leonardasf1 <leonardasf1@yandex.ru>
2022-09-28 13:24:05 +0300

    upgrade Menu

--- components.d.ts
+++ components.d.ts
@@ -8,11 +8,16 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElRadioButton: typeof import('element-plus/es')['ElRadioButton']
+    ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElRow: typeof import('element-plus/es')['ElRow']
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     Graph: typeof import('./src/components/Graph.vue')['default']
     HeaderCom: typeof import('./src/components/HeaderCom.vue')['default']
+    IconArrow: typeof import('./src/components/IconArrow.vue')['default']
     List: typeof import('./src/components/List.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/components/TabMenu.vue
+++ src/components/TabMenu.vue
@@ -1,25 +1,83 @@
 <script setup lang="ts">
 
-const props = defineProps<{
-  charts: Array<any>
-  handleC: any
-}>()
+  import { ref } from 'vue'
+  import {
+    Document,
+    Menu as IconMenu,
+    Location,
+    Setting,
+  } from '@element-plus/icons-vue'
 
+  import IconArrow from '@/components/IconArrow.vue'
+
+  const isCollapse = ref(true)
+
+  const props = defineProps<{
+    charts: Array<any>
+    handleC: any
+  }>()
+
+  const styleClose = { transform: "rotateZ(90deg)" }
+  const styleOpen = { transform: "rotateZ(-90deg)" }
 </script>
 
 <template>
   <el-menu
+    default-active="1"
     active-text-color="#ffd04b"
     background-color="#545c64"
     class="el-menu-vertical-demo"
     text-color="#fff"
+    :collapse="isCollapse"
   >
-    <el-menu-item
-      v-for="tab in charts"
-      :index="tab.id+''"
-      @click="handleC(tab)"
+    <el-radio-group v-model="isCollapse">
+      <el-radio-button :label="false">
+        <icon-arrow :style="styleOpen" />
+      </el-radio-button>
+      <el-radio-button :label="true">
+        <icon-arrow :style="styleClose" />
+      </el-radio-button>
+    </el-radio-group>
+    <el-sub-menu
+      v-for="(tab, tabIndex) in charts"
+      :index="tab.id+'1'"
     >
-      {{tab.name}}
-    </el-menu-item>
+      <template #title>
+        <el-icon><icon-menu /></el-icon>
+        <span> {{tab.name}} </span>
+      </template>
+      <el-menu-item
+        v-for="(de_ref, deIndex) in tab.de_refs"
+        :index="de_ref.id+'2'"
+        @click="handleC(tab, deIndex + 1)"
+      > {{de_ref.name}} </el-menu-item>
+    </el-sub-menu>
   </el-menu>
 </template>
+
+<style lang="scss">
+  .el-radio-group {
+    display: block;
+    height: 48px;
+  }
+  .el-radio-button__inner {
+    background: inherit;
+    border: none;
+    color: #fff;
+    padding: 0;
+    border-radius: 0;
+
+    &:hover {
+      color: #fff;
+    }
+  }
+  .el-radio-button__original-radio:checked + .el-radio-button__inner {
+    display: none;
+  }
+  .el-radio-button:first-child .el-radio-button__inner {
+    border-left: 0;
+  }
+  .el-sub-menu__title span {
+    padding-inline: 20px;
+  }
+</style>
